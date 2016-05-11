b2501ad2c10b55ff29ef49615468eecfb7fcdb74
Azder81 <Azder@mail.ru>
2016-05-11 14:46:59 +0700

    new Activity DetailActivity

---
+++ app/src/main/java/com/example/agogolev/moneytracker/ui/DetailActivity.java
@@ -0,0 +1,14 @@
+package com.example.agogolev.moneytracker.ui;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+
+    }
+}
