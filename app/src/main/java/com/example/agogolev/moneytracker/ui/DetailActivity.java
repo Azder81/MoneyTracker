9e7146664aacf6c167cefc148f0678de758e96fa
Azder81 <Azder@mail.ru>
2016-05-12 15:29:51 +0700

    create Spiner

--- app/src/main/java/com/example/agogolev/moneytracker/ui/DetailActivity.java
+++ app/src/main/java/com/example/agogolev/moneytracker/ui/DetailActivity.java
@@ -2,25 +2,49 @@ package com.example.agogolev.moneytracker.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
 
 import com.example.agogolev.moneytracker.R;
 
+import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.ViewById;
-import org.androidannotations.annotations.ViewsById;
 
 @EActivity(R.layout.activity_detail)
 public class DetailActivity extends AppCompatActivity {
 
-    @ViewById(R.id.sum)
-    EditText editSum;
-    @ViewById(R.id.note)
-    EditText editNote;
-    @ViewById(R.id.spinner)
+    @ViewById
+    EditText sum;
+    @ViewById
+    EditText note;
+    @ViewById
     Spinner spinner;
     @ViewById(R.id.date_expense)
     EditText editDate;
+
+    String[] data = {"Food", "clothes", "communication", "For a car"};
+
+    @AfterViews
+    public void createSpiner() {
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, data);
+        adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setPrompt("Категории");
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
 
 }
