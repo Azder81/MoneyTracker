a67ea119a64df4f17c3e660fd22ac1413248fc97
Azder81 <Azder@mail.ru>
2016-05-14 15:38:40 +0700

    create DatePickerFragment

--- app/src/main/java/com/example/agogolev/moneytracker/ui/DetailActivity.java
+++ app/src/main/java/com/example/agogolev/moneytracker/ui/DetailActivity.java
@@ -1,21 +1,29 @@
 package com.example.agogolev.moneytracker.ui;
 
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.example.agogolev.moneytracker.R;
+import com.example.agogolev.moneytracker.utils.DatePickerFragment;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.ViewById;
 
+import java.util.Calendar;
+
 @EActivity(R.layout.activity_detail)
 public class DetailActivity extends AppCompatActivity {
+
+    public int dd, mm, yy;
 
     @ViewById
     EditText sum;
@@ -24,7 +32,7 @@ public class DetailActivity extends AppCompatActivity {
     @ViewById
     Spinner spinner;
     @ViewById(R.id.date_expense)
-    EditText editDate;
+    TextView editDate;
 
     String[] data = {"Food", "clothes", "communication", "For a car"};
 
@@ -45,6 +53,24 @@ public class DetailActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    @AfterViews
+    public void createDateText() {
+        Calendar cal = Calendar.getInstance();
+        yy = cal.get(Calendar.YEAR);
+        mm = cal.get(Calendar.MONTH);
+        dd = cal.get(Calendar.DAY_OF_MONTH);
+        editDate.setText(new StringBuilder()
+                .append(dd).append(".")
+                .append(mm + 1).append(".")
+                .append(yy)
+        );
+    }
+
+    public void onClickDate(View view) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
     }
 
 }
