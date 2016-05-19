0e882bf380f575cc9b1f9334f1d979afd5bd6a71
Azder81 <Azder@mail.ru>
2016-05-19 17:02:34 +0700

    add ExpensesTable

---
+++ app/src/main/java/com/example/agogolev/moneytracker/database/dbmodels/CategoriesTable.java
@@ -0,0 +1,11 @@
+package com.example.agogolev.moneytracker.database.dbmodels;
+
+import com.example.agogolev.moneytracker.database.MoneyTrackerDatabase;
+import com.raizlabs.android.dbflow.annotation.Table;
+import com.raizlabs.android.dbflow.structure.BaseModel;
+
+@Table(database = MoneyTrackerDatabase.class)
+public class CategoriesTable extends BaseModel {
+
+
+}
