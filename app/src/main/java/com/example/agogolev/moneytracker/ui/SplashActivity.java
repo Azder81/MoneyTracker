808dfa505cb29151b470d9b38ab1c24a906b1bb5
Azder81 <Azder@mail.ru>
2016-06-04 11:43:46 +0700

    добавлена проверка токена из SharedPreferences в SplashActivity

--- app/src/main/java/com/example/agogolev/moneytracker/ui/SplashActivity.java
+++ app/src/main/java/com/example/agogolev/moneytracker/ui/SplashActivity.java
@@ -1,6 +1,8 @@
 package com.example.agogolev.moneytracker.ui;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
@@ -13,14 +15,39 @@ import org.androidannotations.annotations.EActivity;
 public class SplashActivity extends AppCompatActivity {
 
     private final int SPLASH_DISPLAY_LENGTH = 2000;
+    final static String SET_LOGIN = "token";
+    final static String TOKEN = "login token";
+    SharedPreferences sPref;
 
     @AfterViews
-    void startMainActivity() {
+    void initNextActivity() {
+        sPref = getSharedPreferences(SET_LOGIN, Context.MODE_PRIVATE);
+        if (sPref.contains(TOKEN)) {
+            startMainActivity();
+        } else {
+            startLoginActivity();
+        }
+//        SharedPreferences.Editor editor = sPref.edit()
+    }
+
+    void startLoginActivity() {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 /* Create an Intent that will start the Menu-Activity. */
                 Intent mainIntent = new Intent(SplashActivity.this, LoginActivity_.class);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+
+    void startMainActivity() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(SplashActivity.this, MainActivity_.class);
                 SplashActivity.this.startActivity(mainIntent);
                 SplashActivity.this.finish();
             }
